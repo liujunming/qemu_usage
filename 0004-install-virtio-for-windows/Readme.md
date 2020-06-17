3dfe0d9e7e964f50e722a2fee02fc9e372a095a9
Junming Liu <junming.liu@intel.com>
2020-06-17 23:39:15 +0800

    Insert virtio driver for Windows guest
    Signed-off-by: Junming Liu <junming.liu@intel.com>

---
+++ 0004-install-virtio-for-windows/Readme.md
@@ -0,0 +1,5 @@
+Download Windows virtio driver
+
+http://www.zeta.systems/blog/2018/07/03/Installing-Virtio-Drivers-In-Windows-On-KVM/
+
+https://projectacrn.github.io/latest/tutorials/using_windows_as_uos.html
---
+++ 0004-install-virtio-for-windows/insert_virtio.sh
@@ -0,0 +1,14 @@
+#! /bin/bash -x
+    qemu-system-x86_64 \
+    -enable-kvm \
+    -m 2048 \
+    -smp 2 \
+    -cpu host \
+    -cdrom winvirtio.iso \
+    -drive format=raw,file=win10-ltsc.img \
+    -device qemu-xhci,id=xhci \
+    -device usb-host,vendorid=0x05e3,productid=0x0608 \
+    -device usb-host,vendorid=0x15d9,productid=0x0a4e \
+    -net nic,macaddr=00:16:3e:60:0a:50 \
+    -net tap,script=/etc/qemu-ifup
+
